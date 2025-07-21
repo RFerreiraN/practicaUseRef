6e28e9c582e5c719e6483ed19b6d36f28f14a6e0
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-21 22:47:04 +0200

    crearemos un custom hook, para separar logica de vista

--- src/Components/UseRefComponent.jsx
+++ src/Components/UseRefComponent.jsx
@@ -1,23 +1,64 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 export const UseRefComponent = () => {
+
+  const formRef = useRef()
+
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = inputValue;
+
+  const handleInput = ({ target }) => {
+    const { name, value } = target
+    setInputValue({
+      ...inputValue,
+      [name]: value
+    })
+  }
+
+  const resetForm = () => {
+    setInputValue({
+      email: '',
+      password: ''
+    })
+  }
+
+  const onSubmitInput = (event) => {
+    event.preventDefault()
+    if (!inputValue.email.trim() || !inputValue.password.trim()) return
+    console.log(inputValue)
+    resetForm()
+  }
+
+  useEffect(() => {
+    formRef.current.focus()
+  }, [])
+
   return (
-    <form>
+    <form onSubmit={onSubmitInput}>
       <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input 
-          type="email" 
-          className="form-control" 
-          name="email" 
+        <input
+          ref={formRef}
+          type="email"
+          className="form-control"
+          name="email"
+          value={email}
+          onChange={handleInput}
         />
       </div>
-      
+
       <div className="mb-3">
         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input 
-          type="password" 
-          className="form-control" 
-          name="password" 
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          value={password}
+          onChange={handleInput}
         />
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
